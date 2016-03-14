e3130a8384c3c8b00380ec88fbd44e33b1d82868
wikibady <hysuperliu@wikibady.com>
2016-03-14 13:17:05 +0800

    css and js

---
+++ static/main.js
@@ -0,0 +1,65 @@
+var sectionList=document.getElementsByTagName('section');
+
+
+
+
+$('section').css(
+    {
+        "height":$(window).height(),
+    }
+)
+$(".nav-button").click(function(){
+        var oLink=document.getElementById("link");
+        var oStyle = oLink.currentStyle? oLink.currentStyle : window.getComputedStyle(oLink, null);
+
+        if(oStyle.display=="none"){
+                oLink.style.display="block";
+        }
+        else{
+            oLink.style.display="none";
+        }
+    }
+)
+function handle(delta) {//handle after wheelEnent
+    var s = delta + ": ";
+    if (delta <0)
+    {
+
+        switch(window.location.hash){
+            case "#home":
+                window.location.hash="about";
+                break;
+            case "#about":
+                window.location.hash="works";
+                break;
+            case "#works":
+                window.location.hash="contact";
+                break;
+            default :
+                break;
+        }
+       //ÏÂ»¬
+    }
+    else {
+        window.location.hash="home";//ÉÏ»¬
+    }
+}
+var bIfScroll =0;
+function wheel(event){//whellEvent
+    var delta = 0;
+    if (!event) event = window.event;
+    if (event.wheelDelta) {
+        delta = event.wheelDelta/120;
+        if (window.opera) delta = -delta;
+    } else if (event.detail) {
+        delta = -event.detail/3;
+    }
+    if (delta && a%2==0)
+        handle(delta);
+    bIfScroll++;
+}
+
+//add whellEvent
+if (window.addEventListener)
+    window.addEventListener('DOMMouseScroll', wheel, false);
+window.onmousewheel = document.onmousewheel = wheel;
